5718b4362b732dc671e96d281d7b7adb35694dc5
Sahith <84251873+sahithgondi@users.noreply.github.com>
2025-02-02 22:08:24 -0600

    Update Index.java
    mwahaha

--- src/main/java/pro/pantrypilot/endpoints/pages/Index.java
+++ src/main/java/pro/pantrypilot/endpoints/pages/Index.java
@@ -16,7 +16,7 @@ public class Index implements HttpHandler {
         try{
 //            String content = new String(Files.readAllBytes(Paths.get("/home/jlw1808/crtapi/src/main/java/index.html")));
 //            sb.append(content);
-            sb.append("Hello World!");
+            sb.append("Gooners Unite!");
         }
         catch (Exception e){
             e.printStackTrace();
