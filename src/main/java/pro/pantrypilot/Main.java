a18503ac283a3aae4a382201dd2b29d2b12441b2
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-03 10:26:17 -0600

    initialize the databases on start

--- src/main/java/pro/pantrypilot/Main.java
+++ src/main/java/pro/pantrypilot/Main.java
@@ -2,6 +2,7 @@ package pro.pantrypilot;
 
 import com.sun.net.httpserver.HttpServer;
 import pro.pantrypilot.config.ConfigurationManager;
+import pro.pantrypilot.db.DatabaseConnectionManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -11,6 +12,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ConfigurationManager.loadConfig();
+
+        DatabaseConnectionManager.connectToDatabase();
+        DatabaseConnectionManager.initializeDatabase();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(ConfigurationManager.getIntProperty("server.port")), 0);
 
