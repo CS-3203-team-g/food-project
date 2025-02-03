243ce9150ad9e9c7a69b06d94cbffc0785965bc8
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-02 23:12:53 -0600

    Add base website code and a landing page

--- src/main/java/pro/pantrypilot/endpoints/pages/Index.java
+++ src/main/java/pro/pantrypilot/endpoints/pages/Index.java
@@ -5,30 +5,24 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Index implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
-        StringBuilder sb = new StringBuilder();
-
-        try{
-//            String content = new String(Files.readAllBytes(Paths.get("/home/jlw1808/crtapi/src/main/java/index.html")));
-//            sb.append(content);
-            sb.append("Hello World!");
-        }
-        catch (Exception e){
+        String response;
+        try {
+            response = new String(Files.readAllBytes(Paths.get("src/main/resources/static/index.html")));
+        } catch (IOException e) {
             e.printStackTrace();
+            response = "Error: Unable to load index.html";
         }
 
-        String response = sb.toString();
-
-        // send file to user
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
-
     }
 }
