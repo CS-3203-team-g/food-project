ad5500b90c2fda85b5c8b539c248a411354db6ab
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-04 22:27:33 -0600

    Create Recipe.java

---
+++ src/main/java/pro/pantrypilot/db/classes/recipe/Recipe.java
@@ -0,0 +1,76 @@
+package pro.pantrypilot.db.classes.recipe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+public class Recipe {
+
+    private static final Logger logger = LoggerFactory.getLogger(Recipe.class);
+
+    private final int recipeID;
+    private final String title;
+    private final String thumbnailUrl;
+    private final String instructions;
+    private ArrayList<RecipeIngredient> ingredients;
+    private final float rating;
+
+
+    public Recipe(int recipeID, String title, String thumbnailUrl, String instructions, ArrayList<RecipeIngredient> ingredients, float rating) {
+        this.recipeID = recipeID;
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+        this.rating = rating;
+    }
+
+    public Recipe(int recipeID, String title, String thumbnailUrl, String instructions, float rating) {
+        this.recipeID = recipeID;
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+        this.instructions = instructions;
+        this.rating = rating;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public ArrayList<RecipeIngredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<RecipeIngredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "recipeID=" + recipeID +
+                ", title='" + title + '\'' +
+                ", thumbnailUrl='" + thumbnailUrl + '\'' +
+                ", instructions='" + instructions + '\'' +
+                ", ingredients=" + ingredients +
+                ", rating=" + rating +
+                '}';
+    }
+}
