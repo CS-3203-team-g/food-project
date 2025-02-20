96555284b77e6814d47c784d5f2b1e45ac6bab2f
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-20 09:08:12 -0600

    Update endpoints to use FileHelper

--- src/main/java/pro/pantrypilot/endpoints/pages/settings/Settings.java
+++ src/main/java/pro/pantrypilot/endpoints/pages/settings/Settings.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pro.pantrypilot.helpers.FileHelper;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -20,7 +21,7 @@ public class Settings implements HttpHandler {
         byte[] responseBytes;
         try {
             // Read the file as bytes and then convert to String with UTF-8 encoding
-            byte[] fileBytes = Files.readAllBytes(Paths.get("src/main/resources/static/settings/settings.html"));
+            byte[] fileBytes = FileHelper.readFile("static/settings/settings.html");
             String response = new String(fileBytes, StandardCharsets.UTF_8);
             responseBytes = response.getBytes(StandardCharsets.UTF_8);
         } catch (IOException e) {
