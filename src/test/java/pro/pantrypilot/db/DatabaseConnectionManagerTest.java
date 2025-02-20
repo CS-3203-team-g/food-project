a0fa49244d40c6febbb5eb25b15f9e37badaab3c
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-20 00:07:59 -0600

    remove debug print statements

--- src/test/java/pro/pantrypilot/db/DatabaseConnectionManagerTest.java
+++ src/test/java/pro/pantrypilot/db/DatabaseConnectionManagerTest.java
@@ -9,18 +9,10 @@ class DatabaseConnectionManagerTest {
     @Test
     void testGetConnection() {
         try {
-            System.out.println("TESTING");
             Connection conn = DatabaseConnectionManager.getConnection();
-            System.out.println("TESTING");
             assertNotNull(conn, "Connection should not be null");
-            System.out.println("TESTING");
             conn.close();
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("AKSJHBDKASJD");
-            System.out.println(e.getMessage());
-            System.out.println(e.getCause());
-            System.out.println(e.getClass());
             fail("Should not throw exception: " + e.getMessage());
         }
     }
